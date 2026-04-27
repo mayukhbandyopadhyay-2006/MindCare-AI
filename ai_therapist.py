f40dea07e0c645a0287ab382529f6c7bc7e088fa
mayukhbandyopadhyay-2006 <bandyopadhyaymayukh47@gmail.com>
2026-04-27 23:27:39 +0530

    Create ai_therapist.py

---
+++ ai_therapist.py
@@ -0,0 +1,10 @@
+def respond(text, mood):
+
+    if mood == "sad":
+        return "I understand you are feeling sad. Would you like to talk about it?"
+
+    elif mood == "suicidal":
+        return "Please contact India's mental health helpline 9152987821"
+
+    else:
+        return "Tell me more about how you feel."
